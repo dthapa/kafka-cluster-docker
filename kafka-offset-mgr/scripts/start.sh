13003485209d1bfd2b844f242fa25dfdee8da6cf
dthapa <donaththapa@gmail.com>
2015-07-24 01:16:02 -0500

    removing specific ip because of icc

--- kafka-offset-mgr/scripts/start.sh
+++ kafka-offset-mgr/scripts/start.sh
@@ -1,6 +1,6 @@
 java -cp KafkaOffsetMonitor-assembly-0.2.1.jar \
      com.quantifind.kafka.offsetapp.OffsetGetterWeb \
-     --zk 192.168.1.158:2181 \
+     --zk localhost:2181 \
      --port 8081 \
      --refresh 10.seconds \
      --retain 2.days
